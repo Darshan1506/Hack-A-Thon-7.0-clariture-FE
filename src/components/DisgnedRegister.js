468742c70ab417c78631c7eba7964eddb949838f
Darshan <darshanrajpurohit47@gmail.com>
2023-03-20 19:35:00 +0530

    package1

--- src/components/DisgnedRegister.js
+++ src/components/DisgnedRegister.js
@@ -51,10 +51,10 @@ const DisgnedRegister = () => {
       <path d="M80 192V144C80 64.47 144.5 0 224 0C303.5 0 368 64.47 368 144V192H384C419.3 192 448 220.7 448 256V448C448 483.3 419.3 512 384 512H64C28.65 512 0 483.3 0 448V256C0 220.7 28.65 192 64 192H80zM144 192H304V144C304 99.82 268.2 64 224 64C179.8 64 144 99.82 144 144V192z"></path></svg>
       <input autocomplete="off" id="logpass" placeholder="Password" class="input-field" name="logpass" type="password" onChange={(e) => setPassword(e.target.value)}></input>
     </div>
-    <button class="btn" type="submit">Login</button>
+    <button class="btn" type="submit">Register</button>
   </form>
   <div className="p-2 box mt-2 text-center btn-sig">
-  Already have an account? <Link to="/">Register</Link>
+  Already have an account? <Link to="/">Log In</Link>
       </div>
 </div>
     </div>
